adf6636f1858cdd19e154615be35e6dda4d72061
Hope Giometti <hopegiometti@gmail.com>
2019-09-18 19:12:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["a", "b", "c", "d"]
-  array.push("e")
+  array.push("#{element}element")
 end
 
 def add_element_to_start_of_array(array, element)
